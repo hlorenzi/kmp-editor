023ae99002f61ce04eb9d129fbef8c257fec74ef
Epik95 <epik955@gmail.com>
2023-02-24 01:30:34 -0500

    Fix model transparency issues

--- src/util/kclLoader.js
+++ src/util/kclLoader.js
@@ -3,40 +3,41 @@ const { ModelBuilder } = require("./modelBuilder.js")
 const { Vec3 } = require("../math/vec3.js")
 
 
+const cat = (isDeath, isInvis, isEffect, isWall, isItem) => { return { isDeath, isInvis, isEffect, isWall, isItem }}
 const collisionTypeData =
 [
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [1.0, 1.0, 1.0, 1.0], name: "Road"                      },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [1.0, 0.9, 0.8, 1.0], name: "Slippery Road (sand/dirt)" },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.0, 0.8, 0.0, 1.0], name: "Weak Off-Road"             },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.0, 0.6, 0.0, 1.0], name: "Off-Road"                  },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.0, 0.4, 0.0, 1.0], name: "Heavy Off-Road"            },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.8, 0.9, 1.0, 1.0], name: "Slippery Road (ice)"       },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [1.0, 0.5, 0.0, 1.0], name: "Boost Panel"               },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [1.0, 0.6, 0.0, 1.0], name: "Boost Ramp"                },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [1.0, 0.8, 0.0, 1.0], name: "Slow Ramp"                 },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: true,  c: [0.9, 0.9, 1.0, 0.5], name: "Item Road"                 },
-	{ isDeath: true,  isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.7, 0.1, 0.1, 1.0], name: "Solid Fall"                },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.0, 0.5, 1.0, 1.0], name: "Moving Water"              },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: true,  isItem: false, c: [0.6, 0.6, 0.6, 1.0], name: "Wall"                      },
-	{ isDeath: false, isInvis: true,  isEffect: false, isWall: true,  isItem: false, c: [0.0, 0.0, 0.6, 0.8], name: "Invisible Wall"            },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: true,  c: [0.6, 0.6, 0.7, 0.5], name: "Item Wall"                 },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: true,  isItem: false, c: [0.6, 0.6, 0.6, 1.0], name: "Wall 2"                    },
-	{ isDeath: true,  isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.8, 0.0, 0.0, 0.8], name: "Fall Boundary"             },
-	{ isDeath: false, isInvis: false, isEffect: true,  isWall: false, isItem: false, c: [1.0, 0.0, 0.5, 0.8], name: "Cannon Activator"          },
-	{ isDeath: false, isInvis: false, isEffect: true,  isWall: false, isItem: false, c: [0.5, 0.0, 1.0, 0.5], name: "Force Recalculation"       },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.0, 0.3, 1.0, 1.0], name: "Half-pipe Ramp"            },
-	{ isDeath: false, isInvis: true,  isEffect: false, isWall: true,  isItem: false, c: [0.8, 0.4, 0.0, 0.8], name: "Player-Only Wall"          },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.9, 0.9, 1.0, 1.0], name: "Moving Road"               },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.9, 0.7, 1.0, 1.0], name: "Sticky Road"               },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [1.0, 1.0, 1.0, 1.0], name: "Road 2"                    },
-	{ isDeath: false, isInvis: false, isEffect: true,  isWall: false, isItem: false, c: [1.0, 0.0, 1.0, 0.8], name: "Sound Trigger"             },
-	{ isDeath: false, isInvis: true , isEffect: false, isWall: true,  isItem: false, c: [0.4, 0.6, 0.4, 0.8], name: "Weak Wall"                 },
-	{ isDeath: false, isInvis: false, isEffect: true,  isWall: false, isItem: false, c: [0.8, 0.0, 1.0, 0.8], name: "Effect Trigger"            },
-	{ isDeath: false, isInvis: false, isEffect: true,  isWall: false, isItem: false, c: [1.0, 0.0, 1.0, 0.5], name: "Item State Modifier"       },
-	{ isDeath: false, isInvis: true,  isEffect: false, isWall: true,  isItem: false, c: [0.0, 0.6, 0.0, 0.8], name: "Half-pipe Invis Wall"      },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: false, isItem: false, c: [0.9, 0.9, 1.0, 1.0], name: "Rotating Road"             },
-	{ isDeath: false, isInvis: false, isEffect: false, isWall: true,  isItem: false, c: [0.8, 0.7, 0.8, 1.0], name: "Special Wall"              },
-	{ isDeath: false, isInvis: true,  isEffect: false, isWall: true,  isItem: false, c: [0.0, 0.0, 0.6, 0.8], name: "Invisible Wall 2"          },
+	{ f: cat(0, 0, 0, 0, 0), c: [1.0, 1.0, 1.0, 1.0], priority: 0, name: "Road"                      },
+	{ f: cat(0, 0, 0, 0, 0), c: [1.0, 0.9, 0.8, 1.0], priority: 0, name: "Slippery Road (sand/dirt)" },
+	{ f: cat(0, 0, 0, 0, 0), c: [0.0, 0.8, 0.0, 1.0], priority: 0, name: "Weak Off-Road"             },
+	{ f: cat(0, 0, 0, 0, 0), c: [0.0, 0.6, 0.0, 1.0], priority: 0, name: "Off-Road"                  },
+	{ f: cat(0, 0, 0, 0, 0), c: [0.0, 0.4, 0.0, 1.0], priority: 0, name: "Heavy Off-Road"            },
+	{ f: cat(0, 0, 0, 0, 0), c: [0.8, 0.9, 1.0, 1.0], priority: 0, name: "Slippery Road (ice)"       },
+	{ f: cat(0, 0, 0, 0, 0), c: [1.0, 0.5, 0.0, 1.0], priority: 0, name: "Boost Panel"               },
+	{ f: cat(0, 0, 0, 0, 0), c: [1.0, 0.6, 0.0, 1.0], priority: 0, name: "Boost Ramp"                },
+	{ f: cat(0, 0, 0, 0, 0), c: [1.0, 0.8, 0.0, 1.0], priority: 0, name: "Slow Ramp"                 },
+	{ f: cat(0, 0, 0, 0, 1), c: [0.9, 0.9, 1.0, 0.5], priority: 2, name: "Item Road"                 },
+	{ f: cat(1, 0, 0, 0, 0), c: [0.7, 0.1, 0.1, 1.0], priority: 0, name: "Solid Fall"                },
+	{ f: cat(0, 0, 0, 0, 0), c: [0.0, 0.5, 1.0, 1.0], priority: 0, name: "Moving Water"              },
+	{ f: cat(0, 0, 0, 1, 0), c: [0.6, 0.6, 0.6, 1.0], priority: 0, name: "Wall"                      },
+	{ f: cat(0, 1, 0, 1, 0), c: [0.0, 0.0, 0.6, 0.8], priority: 3, name: "Invisible Wall"            },
+	{ f: cat(0, 0, 0, 0, 1), c: [0.6, 0.6, 0.7, 0.5], priority: 2, name: "Item Wall"                 },
+	{ f: cat(0, 0, 0, 1, 0), c: [0.6, 0.6, 0.6, 1.0], priority: 0, name: "Wall 2"                    },
+	{ f: cat(1, 0, 0, 0, 0), c: [0.8, 0.0, 0.0, 0.8], priority: 4, name: "Fall Boundary"             },
+	{ f: cat(0, 0, 1, 0, 0), c: [1.0, 0.0, 0.5, 0.8], priority: 1, name: "Cannon Activator"          },
+	{ f: cat(0, 0, 1, 0, 0), c: [0.5, 0.0, 1.0, 0.5], priority: 1, name: "Force Recalculation"       },
+	{ f: cat(0, 0, 0, 0, 0), c: [0.0, 0.3, 1.0, 1.0], priority: 0, name: "Half-pipe Ramp"            },
+	{ f: cat(0, 1, 0, 1, 0), c: [0.8, 0.4, 0.0, 0.8], priority: 1, name: "Player-Only Wall"          },
+	{ f: cat(0, 0, 0, 0, 0), c: [0.9, 0.9, 1.0, 1.0], priority: 0, name: "Moving Road"               },
+	{ f: cat(0, 0, 0, 0, 0), c: [0.9, 0.7, 1.0, 1.0], priority: 0, name: "Sticky Road"               },
+	{ f: cat(0, 0, 0, 0, 0), c: [1.0, 1.0, 1.0, 1.0], priority: 0, name: "Road 2"                    },
+	{ f: cat(0, 0, 1, 0, 0), c: [1.0, 0.0, 1.0, 0.8], priority: 1, name: "Sound Trigger"             },
+	{ f: cat(0, 1, 0, 1, 0), c: [0.4, 0.6, 0.4, 0.8], priority: 1, name: "Weak Wall"                 },
+	{ f: cat(0, 0, 1, 0, 0), c: [0.8, 0.0, 1.0, 0.8], priority: 1, name: "Effect Trigger"            },
+	{ f: cat(0, 0, 1, 0, 0), c: [1.0, 0.0, 1.0, 0.5], priority: 1, name: "Item State Modifier"       },
+	{ f: cat(0, 1, 0, 1, 0), c: [0.0, 0.6, 0.0, 0.8], priority: 3, name: "Half-pipe Invis Wall"      },
+	{ f: cat(0, 0, 0, 0, 0), c: [0.9, 0.9, 1.0, 1.0], priority: 0, name: "Rotating Road"             },
+	{ f: cat(0, 0, 0, 1, 0), c: [0.8, 0.7, 0.8, 1.0], priority: 0, name: "Special Wall"              },
+	{ f: cat(0, 1, 0, 1, 0), c: [0.0, 0.0, 0.6, 0.8], priority: 3, name: "Invisible Wall 2"          },
 ]
 
 
@@ -50,6 +51,8 @@ class KclLoader
 		let section2Offset = parser.readUInt32()
 		let section3Offset = parser.readUInt32()
 		let section4Offset = parser.readUInt32()
+
+		let triLists = [ [], [], [], [], [] ]
 		
 		let vertices = []
 		parser.seek(section1Offset)
@@ -126,23 +129,23 @@ class KclLoader
 				(hl.collisionEffect == -1 || ((collisionFlags >>> 13) & 0x7) == hl.collisionEffect))
 			{
 				let color = [1, 1, 0, 1]
-				model.addTri(v1, v2, v3, color, color, color)
+				triLists[data.priority].push({ v1, v2, v3, color })
 				continue
 			}
 
-			if (cfg && data.isWall && cfg.kclEnableWalls !== undefined && !cfg.kclEnableWalls)
+			if (cfg && data.f.isWall && cfg.kclEnableWalls !== undefined && !cfg.kclEnableWalls)
 				continue
 
-			if (cfg && data.isDeath && cfg.kclEnableDeathBarriers !== undefined && !cfg.kclEnableDeathBarriers)
+			if (cfg && data.f.isDeath && cfg.kclEnableDeathBarriers !== undefined && !cfg.kclEnableDeathBarriers)
 				continue
 			
-			if (cfg && data.isInvis && cfg.kclEnableInvisible !== undefined && !cfg.kclEnableInvisible)
+			if (cfg && data.f.isInvis && cfg.kclEnableInvisible !== undefined && !cfg.kclEnableInvisible)
 				continue
 
-			if (cfg && data.isItem && cfg.kclEnableItemRoad !== undefined && !cfg.kclEnableItemRoad)
+			if (cfg && data.f.isItem && cfg.kclEnableItemRoad !== undefined && !cfg.kclEnableItemRoad)
 				continue
 			
-			if (cfg && data.isEffect && cfg.kclEnableEffects !== undefined && !cfg.kclEnableEffects)
+			if (cfg && data.f.isEffect && cfg.kclEnableEffects !== undefined && !cfg.kclEnableEffects)
 				continue
 			
 			let color = data.c
@@ -162,21 +165,24 @@ class KclLoader
 						let v1to2 = v2.sub(v1)
 						let v1to3 = v3.sub(v1)
 						let normal = v1to2.cross(v1to3).normalize()
-						highlighted = data.isWall && normal.dot(new Vec3(0, 0, 1)) > 0.9
+						highlighted = data.f.isWall && normal.dot(new Vec3(0, 0, 1)) > 0.9
 						break
 
 					case 3:
-						highlighted = data.isWall && collisionFlags & 0x8000
+						highlighted = data.f.isWall && collisionFlags & 0x8000
 						break
 				}
 
 				if (highlighted)
 					color = [1.0, 1.0, 0.0, 1.0]
 			}
-			
-			model.addTri(v1, v2, v3, color, color, color)
+			triLists[data.priority].push({ v1, v2, v3, color })
 		}
-		
+
+		for (let lis of triLists)
+			for (let tri of lis)
+				model.addTri(tri.v1, tri.v2, tri.v3, tri.color, tri.color, tri.color)
+
 		return model.calculateNormals()
 	}
 }
